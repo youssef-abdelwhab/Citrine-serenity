a26b6b81dc1cd37c8e2389c11e6e8515cf60ec23
youssef-abdelwhab <youssef5abdelwahab@gmail.com>
2025-04-15 23:24:27 +0100

    add slide cart

---
+++ js/mian.js
@@ -0,0 +1,8 @@
+let slide_cart = document.getElementById("cart")
+function open_cart(){
+    slide_cart.classList.add("actev")
+}
+function close_cart(){
+    slide_cart.classList.remove("actev")
+}
+
