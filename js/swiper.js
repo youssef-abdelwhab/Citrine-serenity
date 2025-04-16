6c49a6762cdf8041a8ec6eca5d22b735d7491c20
youssef-abdelwhab <youssef5abdelwahab@gmail.com>
2025-04-16 19:00:52 +0100

    add prodcuts whit img and servis and banner

--- js/swiper.js
+++ js/swiper.js
@@ -26,6 +26,37 @@ const swiper = new Swiper('.swiper', {
     loop:true,
     breakpoints:{
       1600:{
+        slidesPerView: 4,
+      },
+      1200:{
+        slidesPerView: 3,
+        spaceBetween:25,
+      },
+      700:{
+        slidesPerView: 3,
+        spaceBetween:15,
+      },
+      0:{
+        slidesPerView: 2,
+        spaceBetween:10,
+      },
+    }
+  });
+
+  
+  var swiper2 = new Swiper(".sale_product", {
+    slidesPerView: 4,
+    spaceBetween:30,
+    // autoplay:{
+    //    delay:4000,
+    // },
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev"
+    },
+    loop:true,
+    breakpoints:{
+      1600:{
         slidesPerView: 5,
       },
       1200:{
